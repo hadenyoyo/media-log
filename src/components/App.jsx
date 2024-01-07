344cc82757391355c0d229b2277aebed6be68204
Haden Blackwell <10506594+hadenyoyo@users.noreply.github.com>
2024-01-06 22:27:23 -0600

    First commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import Nav from "./Nav"
+import ReviewTemplate from "./ReviewTemplate"
+
+export default function App () {
+    return (
+        <div>
+            <Nav />
+            <ReviewTemplate />
+        </div>
+    ) 
+}
---
+++ src/components/Nav.jsx
@@ -0,0 +1,8 @@
+export default function Nav () {
+    return (
+            <nav>
+                <h1>Haden&#39;s Media Log</h1>
+                <h1 className="about-button">About</h1>
+            </nav>
+    ) 
+}
